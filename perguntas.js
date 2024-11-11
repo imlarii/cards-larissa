81bbbce6588d2fe5dba249dcadcb675c4c86013d
imlarii <161321070+imlarii@users.noreply.github.com>
2024-11-11 07:54:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais próximo do sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Galáxia',
+    'Qual galáxia o sistema solar está localizado?',
+    'Via Láctea'
+)
+
+criaCartao(
+    'Estrela',
+    'O que é uma Supernova?',
+    'Explosão de uma estrela massiva ao final de sua vida'
+)
+
+criaCartao(
+    'Planetas',
+    'Qual o planeta conhecido como "Planeta Vermelho"?',
+    'Marte'
+)
+
+criaCartao(
+    'Fenômenos',
+    'Como se chama o fenômeno em que a Lua bloqueia a luz do Sol?',
+    'Eclipse Solar'
+)
+criaCartao(
+    'História',
+    'Em que ano o homem pisou na Lua pela primeira vez?',
+    '1969'
+)
+
+criaCartao(
+    'Planetas',
+    'Qual o nome do planeta mais distante do sol no sistema solar?',
+    'Netuno'
+)
+
+criaCartao(
+    'Fases da Lua',
+    'Qual a fase em que a lua está totalmente iluminada?',
+    'Lua Cheia'
+)
+
+criaCartao(
+    'Planetas',
+    'Qual planeta possui anéis mais visíveis ao redor dele?',
+    'Saturno'
+)
